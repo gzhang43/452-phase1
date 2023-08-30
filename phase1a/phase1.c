3e497fd0ac9bab0092e7c08be102bcc24c9cd53a
Grace Zhang <gracezhang@dhcp-10-134-178-158.uawifi.arizona.edu>
2023-08-30 13:16:36 -0700

    added phase1.c and started required function definitions

---
+++ phase1a/phase1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "phase1.h"
+
+void phase1_init(void) {
+
+}
+
+void startProcesses(void) {
+    
+}
+
+void init_main(void) {
+
+}
+
+int fork1(char *name, int(*func)(char *), char *arg, int stacksize,
+        int priority) {
+    
+}
+
+int join(int *status) {
+
+}
+
+void quit(int status, int switchToPid) {
+
+}
+
+int getpid(void) {
+
+}
+
+void dumpProcesses(void) {
+
+}
+
+void TEMP_switchTo(int pid) {
+
+}
