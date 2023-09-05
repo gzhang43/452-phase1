a8486fa04c0abab936be6a00f70163ca67ffe43b
Grace Zhang <gracezhang@Graces-MacBook-Pro.local>
2023-09-05 13:09:28 -0700

    add context switch to init_main()

--- phase1a/phase1.c
+++ phase1a/phase1.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "phase1.h"
 
-void phase1_init(void) {
+typedef struct PCB {
+    USLOSS_Context* context;
+    int pid; 
+} PCB;
 
+PCB processTable[MAXPROC];
+
+void init_main(void) {
+    phase2_start_service_processes();
+    phase3_start_service_processes();
+    phase4_start_service_processes();
+    phase5_start_service_processes();
+}
+
+// Initialize data structures including process table entry for init
+void phase1_init(void) {
+    int pid = 1;
+    struct PCB init;
+    void *stack = malloc(USLOSS_MIN_STACK);
+    
+    init.pid = pid;
+    USLOSS_ContextInit(init.context, stack, USLOSS_MIN_STACK, NULL, &init_main);
+    processTable[pid] = init;
 }
 
 void startProcesses(void) {
-    
-}
-
-void init_main(void) {
-
+    USLOSS_Context *old;
+    USLOSS_ContextSwitch(old, processTable[1].context);
 }
 
 int fork1(char *name, int(*func)(char *), char *arg, int stacksize,
